89757a568fd377d90784ac6fd862e52747fc5733
MihailTS <tseluiko.m@gmail.com>
2018-05-09 18:33:41 +0300

    Task3: implement present method of MarketHtmlPresenter class (fixes #8)

--- src/Task3/MarketHtmlPresenter.php
+++ src/Task3/MarketHtmlPresenter.php
@@ -3,11 +3,23 @@
 namespace Cryptocurrency\Task3;
 
 use Cryptocurrency\Task1\CoinMarket;
+use Cryptocurrency\Task1\Currency;
 
 class MarketHtmlPresenter
 {
     public function present(CoinMarket $market): string
     {
-        // @todo
+        $coinsList = '<ul>';
+        foreach($market->getCurrencies() as $currency){
+            /**
+             * @var Currency $currency
+             */
+            $coinsList .= '<li>' .
+                    "<img src=\"{$currency->getLogoUrl()}\">" .
+                    "<p>{$currency->getName()}: {$currency->getDailyPrice()}</p>" .
+                '</li>';
+        }
+        $coinsList .= '</ul>';
+        return $coinsList;
     }
 }
